ed89f779604bf341c3752cf290e0e649f9f604c3
Rafiqul Islam Tusher <md.rafiqul26997@gmail.com>
2022-06-14 01:41:31 +0600

    added primary btn

--- src/pages/Home/About/About.js
+++ src/pages/Home/About/About.js
@@ -12,7 +12,7 @@ const About = () => {
             <div class="w-full lg:w-[1000px] mx-auto ">
                 <div class="grid grid-cols-1 lg:grid-cols-2 items-center gap-y-6 px-4 lg:px-0 ">
                     <div class="order-last flex flex-col gap-6 text-white">
-                        <h1 class="text-5xl font-bold ">About Me</h1>
+                        <h1 class="section-title ">About Me</h1>
                         <article>
                             I am a Full-Stack Developer. I enjoy creating Responsive applications, in addition, to being user-friendly and developing that backend aspect as well. I have already done a few front-end and full-stack projects and also I am trying to optimize better 
                         </article>
@@ -20,35 +20,13 @@ const About = () => {
                             <HashLink smooth to='/home#projects'>
                                 <button className='btn bg-transparent border text-lg font-normal px-5 mr-5'>MY PROJECT</button>
                             </HashLink>
-                            <button className='btn bg-transparent border text-lg font-normal px-5 '>MY SKILLS</button>
+                            <button className='btn primary-btn '>MY SKILLS</button>
                         </div>
                     </div>
                     <div class="card w-full lg:max-w-sm py-16 px-3 profile-card rounded-none font-normal">
                         <div class="card-body z-10">
                             <div className="text-white">
-                                {/* 
-                                <div className="flex flex-col gap-6">
-                                    <h1>Education: BSc in Computeer Science</h1>
-                                    <div className="">
-                                        <FontAwesomeIcon className='text-[35px] py-0 text-white cursor-pointer' icon={faLocationDot}></FontAwesomeIcon>
-                                        <h1>Dhaka</h1>
-                                        <h1>Bangladesh</h1>
-                                    </div>
-                                    <div className="">
-                                        <Link to=''>
-                                            <FontAwesomeIcon
-                                                className='text-[35px] py-0  text-white cursor-pointer'
-                                                icon={faLinkedin}>
-                                            </FontAwesomeIcon>
-                                        </Link>
-                                        <Link to=''>
-                                            <FontAwesomeIcon
-                                                className='text-[35px] py-0  text-white cursor-pointer'
-                                                icon={faGithub}>
-                                            </FontAwesomeIcon>
-                                        </Link>
-                                    </div>
-                                </div> */}
+                                
                             </div>
                         </div>
                     </div>
--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -5,8 +5,8 @@ import Project from './Project';
 const Projects = () => {
     const projects = useProjects()
     return (
-        <div id='projects'>
-            <h1 className='mt-12 text-[40px] text-primary text-center font-bold'>MY PROJECTS</h1>
+        <div id='projects' className='py-12  bg-[#191919]'>
+            <h1 className='section-title text-primary text-center '>MY PROJECTS</h1>
             <h1 className='mb-12 text-[18px] text-gray-600 text-center '>THOSE I HAVE DONE SUCCESSFULLY</h1>
             <div className="grid max-w-[1428px] mx-auto grid-cols-1 lg:grid-cols-3 justify-items-center gap-12 lg:px-12 px-4">
                 {
